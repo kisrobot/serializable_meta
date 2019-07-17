66b8ee832080ac3726cfb2688031f604f7d896bd
su.sung <su.sung@foxmail.com>
2019-07-17 23:58:51 +0800

    update

--- serializable_meta.go
+++ serializable_meta.go
@@ -7,10 +7,10 @@ import (
 	"fmt"
 	"reflect"
 
-	"github.com/qor/admin"
-	"github.com/qor/qor"
-	"github.com/qor/qor/resource"
-	"github.com/qor/qor/utils"
+	"github.com/kisrobot/admin"
+	"github.com/kisrobot/qor"
+	"github.com/kisrobot/qor/resource"
+	"github.com/kisrobot/qor/utils"
 )
 
 // SerializableMetaInterface is a interface defined methods need for a serializable model
@@ -85,7 +85,7 @@ func (serialize *SerializableMeta) SetSerializableArgumentValue(value interface{
 // ConfigureQorResourceBeforeInitialize configure qor resource for qor admin
 func (serialize *SerializableMeta) ConfigureQorResourceBeforeInitialize(res resource.Resourcer) {
 	if res, ok := res.(*admin.Resource); ok {
-		res.GetAdmin().RegisterViewPath("github.com/qor/serializable_meta/views")
+		res.GetAdmin().RegisterViewPath("github.com/kisrobot/serializable_meta/views")
 
 		if _, ok := res.Value.(SerializableMetaInterface); ok {
 			res.Meta(&admin.Meta{
